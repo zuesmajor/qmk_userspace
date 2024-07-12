d5283b6484daf7369f099f92c537230058be4242
Patrick Ryan <patrick@patrickryan.dev>
2024-07-12 13:35:09 -0700

    Shift calculator functions over one key to the right

--- keyboards/bastardkb/charybdis/3x5/keymaps/via/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/via/keymap.c
@@ -157,7 +157,7 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 #define LAYOUT_LAYER_NUMERAL                                                                  \
     KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, _______________DEAD_HALF_ROW_______________, \
-    KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, KC_SLSH, KC_ASTR, KC_PLUS, KC_MINS, XXXXXXX, \
+    KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, XXXXXXX, KC_SLSH, KC_ASTR, KC_PLUS, KC_MINS, \
      KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, KC_DOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  \
                        KC_DOT,    KC_0, KC_MINS, XXXXXXX, _______
 
