3510794f4cfd11e4f23c3f922c966b61f9909cf2
Patrick Ryan <patrick@patrickryan.dev>
2025-11-24 08:09:25 -0800

    reverting
    ;

--- keyboards/bastardkb/scylla/keymaps/default/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/default/keymap.c
@@ -33,19 +33,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                            │  CTRL  │  SPACE │  MO(1) │                    │  MO(2) │  ENTER │  GUI   │
      *                            └────────┴────────┴────────┘                    └────────┴────────┴────────┘
      *                                     ┌────────┬────────┐                    ┌────────┬────────┐
-     *                                     │  GUI   │  BSPC  │                    │  RSFT  │  ALT   │
+     *                                     │  GUI  │  BSPC  │                    │  GUI   │  ALT   │
      *                                     └────────┴────────┘                    └────────┴────────┘
      */
     [0] = LAYOUT_split_4x6_5(
         // Left Hand
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       // Right Hand
                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
         // Left Thumb Cluster                                    // Right Thumb Cluster
-                 KC_LALT, KC_SPC,  MO(1),                                 MO(2),   KC_ENT,  KC_BSPC,
-                          KC_LGUI, KC_BSPC,                                        KC_RSFT,  KC_PIPE
+                 KC_LSFT, KC_SPC,  MO(1),                                 MO(2),   KC_ENT,  KC_BSPC,
+                          KC_LGUI, KC_BSPC,                                        KC_MINS,  KC_RALT
     ),
 
     /* Layer 1: Numbers & Symbols
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    // Right Hand
                                                                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         QK_BOOT, _______, _______, _______, _______, KC_LBRC,    KC_RBRC, KC_P7,   KC_P8,   KC_P9,   _______, KC_PLUS,
-        _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_LPRN,    KC_RPRN, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, KC_PIPE,
+        _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_LPRN,    KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MINS, KC_PIPE,
         _______, _______, _______, _______, _______, _______,    _______, KC_P1,   KC_P2,   KC_P3,   KC_EQL,  KC_UNDS,
         // Left Thumb Cluster                                    // Right Thumb Cluster
                  KC_LCTL, KC_HOME, KC_TRNS,                               KC_RSFT, KC_RALT, KC_RGUI,
